fcd98eeb86c0db57e793552a781e43aaf01f7c78
Adrian <a.valladares.d@gmail.com>
2023-08-13 14:33:36 +0200

    created donut class

--- receipt_processor/donut.py
+++ receipt_processor/donut.py
@@ -3,79 +3,83 @@ from multiprocessing import Queue, Value
 from loggers import logger
 import torch
 from typing import  Any
-
+import time
 from loggers import logger
 from transformers import DonutProcessor, VisionEncoderDecoderModel # type: ignore
 
 multiprocessingValueAlias: Any
 
-# Define model, processors, etc to be reused
-model_name = "naver-clova-ix/donut-base-finetuned-cord-v2"
-donut_processor = DonutProcessor.from_pretrained(model_name)
-model = VisionEncoderDecoderModel.from_pretrained(model_name)
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model.to(device)
-tensor_type = "pt" # choosing pytorch 
-
-"""
-Decoder_input_ids will be a tensor (PyTorch tensor in this case) containing the token IDs of the task prompt "<s_cord-v2>" 
-after it has been processed by the tokenizer. These decoder_input_ids will later serve as the initial input to the decoder 
-(text generator) part of the image parsing model during the generation process.
-"""
-task_prompt = "<s_cord-v2>" 
-decoder_input_ids = donut_processor.tokenizer(
-    task_prompt,
-    add_special_tokens=False,
-    return_tensors=tensor_type).input_ids
 
 
-def apply_donut_to_image(image):
-    """Gets a PIL Image and runs it though donut. Returns whatever is
-    extracted from said image. The prompt is defaulted, modify the module
-    if you want different prompts"""
-    pixel_values = donut_processor(image, return_tensors=tensor_type).pixel_values
+class Donut:
+    def __init__(self) -> None:
+        model_name = "naver-clova-ix/donut-base-finetuned-cord-v2"
+        self.donut_processor = DonutProcessor.from_pretrained(model_name, use_fast= False)
+        self.model = VisionEncoderDecoderModel.from_pretrained(model_name)
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model.to(self.device)
+        self.tensor_type = "pt" # choosing pytorch 
 
-    outputs = model.generate(
-            pixel_values.to(device),
-                decoder_input_ids=decoder_input_ids.to(device),
-                max_length=model.decoder.config.max_position_embeddings,
-                early_stopping=True,
-                pad_token_id=donut_processor.tokenizer.pad_token_id,
-                eos_token_id=donut_processor.tokenizer.eos_token_id,
-                use_cache=True,
-                num_beams=1,
-                bad_words_ids=[[donut_processor.tokenizer.unk_token_id]],
-                return_dict_in_generate=True,
-            )
+        """
+        Decoder_input_ids will be a tensor (PyTorch tensor in this case) containing the token IDs of the task prompt "<s_cord-v2>" 
+        after it has been processed by the tokenizer. These decoder_input_ids will later serve as the initial input to the decoder 
+        (text generator) part of the image parsing model during the generation process.
+        """
+        task_prompt = "<s_cord-v2>" 
+        self.decoder_input_ids = self.donut_processor.tokenizer(
+            task_prompt,
+            add_special_tokens=False,
+            return_tensors=self.tensor_type).input_ids
 
-    sequence = donut_processor.batch_decode(outputs.sequences)[0]
-    sequence = sequence.replace(donut_processor.tokenizer.eos_token, "").replace(donut_processor.tokenizer.pad_token, "")
-    sequence = re.sub(r"<.*?>", "", sequence, count=1).strip()  # remove first task start token
-    imageInfo = (donut_processor.token2json(sequence))
-    logger.debug(imageInfo)
-    return imageInfo
 
-def runDONUT(inQ: Queue, outQ: Queue, process_counter: multiprocessingValueAlias):
+    def parse_image(self, image):
+        """Gets a PIL Image and runs it though donut. Returns whatever is
+        extracted from said image. The prompt is defaulted, modify the module
+        if you want different prompts"""
+        
+        logger.debug("parse called")
+        pixel_values = self.donut_processor(image, return_tensors=self.tensor_type).pixel_values
+        logger.debug("got pixeñ values")
+        
+        outputs = self.model.generate(
+                pixel_values.to(self.device),
+                    decoder_input_ids=self.decoder_input_ids.to(self.device),
+                    max_length=self.model.decoder.config.max_position_embeddings,
+                    early_stopping=True,
+                    pad_token_id=self.donut_processor.tokenizer.pad_token_id,
+                    eos_token_id=self.donut_processor.tokenizer.eos_token_id,
+                    use_cache=True,
+                    num_beams=1,
+                    bad_words_ids=[[self.donut_processor.tokenizer.unk_token_id]],
+                    return_dict_in_generate=True,
+                )
+        logger.debug("got outputs")
+        sequence = self.donut_processor.batch_decode(outputs.sequences)[0]
+        sequence = sequence.replace(self.donut_processor.tokenizer.eos_token, "").replace(self.donut_processor.tokenizer.pad_token, "")
+        sequence = re.sub(r"<.*?>", "", sequence, count=1).strip()  # remove first task start token
+        imageInfo = (self.donut_processor.token2json(sequence))
+        logger.debug("finished parsing")
+        logger.debug(imageInfo)
+        return imageInfo
+
+
+
+def runDONUT(inQ: Queue, outQ: Queue, process_counter: Any):
     """Gets image from the input Queue and applies the DONUT
         model to it. Image is removed from memory after reading
     """
     logger.debug("DONUT PROCESS STARTED")
     try:
-        processor = DonutProcessor.from_pretrained(
-            "naver-clova-ix/donut-base-finetuned-cord-v2")
-        model = VisionEncoderDecoderModel.from_pretrained(
-            "naver-clova-ix/donut-base-finetuned-cord-v2")
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        model.to(device)
-        
-        tensor_type = "pt"
-
-
+        donut = Donut()
         # while inQ.qsize() > 0:
         while True:
+            logger.debug("WAITING...")
+            time.sleep(1)
             if inQ.qsize()>0:
-                img = inQ.get()
-                img_info = apply_donut_to_image(img)
+                logger.debug("ABOUT TO FETCH IMAGE")
+                receipt_processing_request = inQ.get()
+                logger.debug(f"got request {receipt_processing_request.id}")
+                img_info = donut.parse_image(receipt_processing_request.image)
 
                 logger.debug(img_info)
                 outQ.put(img_info)
@@ -87,6 +91,6 @@ def runDONUT(inQ: Queue, outQ: Queue, process_counter: multiprocessingValueAlias
 
     except Exception as e:
         # Decrement the process count when this process terminates
-        logger.error(f'PROCESS EXIT DUE TO ERROR: {str(e.with_traceback(None))}')
+        logger.error(f'PROCESS EXIT DUE TO ERROR: {str(e)}')
         with process_counter.get_lock():
             process_counter.value -= 1
